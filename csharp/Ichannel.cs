00538e286ad1aab77e7993b89e09a74b43d7db72
qianqians <anxiangqq@gmail.com>
2017-04-05 21:19:35 +0800

    Signed-off-by: qianqians <anxiangqq@gmail.com>

---
+++ csharp/Ichannel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace juggle
+{
+	public interface Ichannel
+    {
+        void disconnect();
+        ArrayList pop();
+        void senddata(byte[] data);
+
+    }
+}
