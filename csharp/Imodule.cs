3ee2fc2ad6a74626281e44f8953cc0e5d2bb4432
qianqians <anxiangqq@gmail.com>
2017-05-23 23:55:21 +0800

    Signed-off-by: qianqians <anxiangqq@gmail.com>

--- csharp/Imodule.cs
+++ csharp/Imodule.cs
@@ -10,34 +10,41 @@ namespace juggle
 		{
 			current_ch = _ch;
 
-			String func_name = (String)_event[1];
+            try
+            {
+                String func_name = (String)_event[1];
 
-			Type type = GetType();
-			MethodInfo method = type.GetMethod(func_name);
+                Type type = GetType();
+                MethodInfo method = type.GetMethod(func_name);
 
-			if (method != null)
-			{
-				try
-				{
-					object[] argv = new object[1];
-					if (_event.Count > 2)
-					{
-						argv[0] = (ArrayList)_event[2];
-					}
-					method.Invoke(this, argv);
+                if (method != null)
+                {
+                    try
+                    {
+                        object[] argv = new object[1];
+                        if (_event.Count > 2)
+                        {
+                            argv[0] = (ArrayList)_event[2];
+                        }
+                        method.Invoke(this, argv);
 
-					current_ch = null;
-				}
-				catch(Exception e)
-				{
-                    throw new juggle.Exception(string.Format("function name:{0} System.Exception:{1}", func_name, e));
-				} 
-			}
-			else
-			{
-                throw new juggle.Exception(string.Format("do not have a function named::{0}", func_name));
-			}
-		}
+                        current_ch = null;
+                    }
+                    catch (Exception e)
+                    {
+                        throw new juggle.Exception(string.Format("function name:{0} System.Exception:{1}", func_name, e));
+                    }
+                }
+                else
+                {
+                    throw new juggle.Exception(string.Format("do not have a function named::{0}", func_name));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new juggle.Exception(string.Format("System.Exception:{0}", e));
+            }
+        }
 
 		public static Ichannel current_ch;
 		public String module_name;
