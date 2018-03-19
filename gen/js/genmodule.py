6a0ffd56f2cd48ccbb7c46beef6e291c57b7cbe6
qianqians <anxiangqq@gmail.com>
2018-03-19 21:32:44 +0800

    Signed-off-by: qianqians <anxiangqq@gmail.com>

--- gen/js/genmodule.py
+++ gen/js/genmodule.py
@@ -6,10 +6,10 @@ def genmodule(module_name, funcs):
         code = "/*this module file is codegen by juggle for js*/\n"
 
         code += "function " + module_name + "_module(){\n"
+        code += "    eventobj.call(this);\n"
         code += "    Imodule.call(this, \"" + module_name + "\");\n\n"
 
         for i in funcs:
-                code += "    this.handle_" + i[1] + " = null;\n"
                 code += "    this." + i[1] + " = function("
                 count = 0
                 for item in i[2]:
@@ -18,13 +18,14 @@ def genmodule(module_name, funcs):
                         if count < len(i[2]):
                                 code += ", "
                 code += "){\n"
-                code += "        if(this.handle_" + i[1] + " !== null){\n"
-                code += "            this.handle_" + i[1] + ".call(null"
+                code += "        this.call_event(\"" + i[1] + "\", ["
                 count = 0
                 for item in i[2]:
-                        code += ", argv" + str(count)
+                        code += "argv" + str(count)
                         count = count + 1
-                code += ");\n"
+                        if count < len(i[2]):
+                                code += ", "
+                code += "]);\n"
                 code += "        }\n"
                 code += "    }\n\n"
         
---
+++ js/eventobj.js
@@ -0,0 +1,13 @@
+function eventobj(){
+    this.events = {}
+
+    this.add_event_listen = function(event, this_argv, mothed){
+        this.events[event] = {"this_argv":this_argv, "mothed":mothed};
+    }
+
+    this.call_event = function(event, argvs){
+        if (this.events[event]){
+            this.events[event]["mothed"].apply(this.events[event]["this_argv"], argvs);
+        }
+    }
+}
