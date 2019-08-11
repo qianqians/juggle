60810a7554840d63511346514ff9d32cbf3746fe
吴铭 <451517996@qq.com>
2019-08-11 14:37:34 +0800

    Signed-off-by: 吴铭 <451517996@qq.com>

--- js/process.js
+++ js/process.js
@@ -1,9 +1,9 @@
 function juggle_process(){
     this.module_set = {};
 
-    this.event_set = [];
-    this.add_event = [];
-    this.remove_event = [];
+    this.event_set = new Array();
+    this.add_event = new Array();
+    this.remove_event = new Array();
 
     this.reg_channel = function(ch){
         this.add_event.push(ch);
@@ -18,17 +18,17 @@ function juggle_process(){
     }
 
     this.poll = function(){
-        for(ch in this.add_event)
+        for(let ch in this.add_event)
         {
             this.event_set.push(this.add_event[ch]);
         }
-        this.add_event = [];
+        this.add_event = new Array();
 
-        var _new_event_set = new [];
-        for(_ch in this.event_set)
+        var _new_event_set = new Array();
+        for(let _ch in this.event_set)
         {
             var in_remove_event = false;
-            for(ch in this.remove_event)
+            for(let ch in this.remove_event)
             {
                 if (this.event_set[_ch] === this.remove_event[ch])
                 {
@@ -42,31 +42,19 @@ function juggle_process(){
             }
         }
         this.event_set = _new_event_set;
-        this.remove_event = [];
+        this.remove_event = new Array();
 
-        _new_event_set = new [];
-        while(this.event_set.length > 0)
+        for(let ch in this.event_set)
         {
-            var ch = this.event_set.shift();
-            
-            var _event = ch.pop();
-            if (_event === null)
-            {
-                _new_event_set.push(ch);
-                continue;
+			while (true)
+			{
+                var _event = this.event_set[ch].pop();
+                if (_event === null)
+                {
+                    break;
+                }
+                this.module_set[_event[0]].process_event(this.event_set[ch], _event);
             }
-            this.module_set[_event[0]].process_event(ch, _event);
-
-            _event = ch.pop();
-            if (_event === null)
-            {
-                _new_event_set.push(ch);
-                continue;
-            }
-            this.module_set[_event[0]].process_event(ch, _event);
-
-            this.event_set.push(ch);
         }
-        this.event_set = _new_event_set;
     }
 }
