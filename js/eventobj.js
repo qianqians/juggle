e857df817bf9e30a1e4c22c1e220dfc4841650f9
吴铭 <451517996@qq.com>
2021-05-13 21:43:40 +0800

    eventobj.js

--- js/eventobj.js
+++ js/eventobj.js
@@ -2,12 +2,23 @@ function eventobj(){
     this.events = {}
 
     this.add_event_listen = function(event, this_argv, mothed){
-        this.events[event] = {"this_argv":this_argv, "mothed":mothed};
+        if (!this.events[event]){
+            this.events[event] = [];
+        }
+        this.events[event].push({"this_argv":this_argv, "mothed":mothed});
     }
 
     this.call_event = function(event, argvs){
-        if (this.events[event] && this.events[event]["mothed"]){
-            this.events[event]["mothed"].apply(this.events[event]["this_argv"], argvs);
+        if (!this.events[event]){
+            return;
+        }
+
+        for(var _event of this.events[event]){
+            if (!_event["mothed"]){
+                continue;
+            }
+            
+            _event["mothed"].apply(_event["mothed"]["this_argv"], argvs);}
         }
     }
 }
