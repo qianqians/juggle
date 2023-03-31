ef1c495d29ddf82300533633f110363f4be200f5
wuming <wuming@lilith.com>
2023-03-31 16:52:58 +0800

    python

---
+++ rpc/gen_common/python/genenum.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+# 2019-12-26
+# build by qianqians
+# genenum
+
+def genenum(pretreatment):
+    enum = pretreatment.enum
+    
+    code = "#coding:utf-8\n"
+    code += "# this enum code is codegen by abelkhan codegen for python\n\n"
+    code += "from enum import Enum"
+    for enum_name, enums in enum.items():
+        code += "class " + enum_name + "(Enum):\n"
+        names = []
+        count = 0
+        for key, value in enums:
+            count = count + 1
+            if key in names:
+                raise Exception("repeat enum elem:%s in enum:%s" % (key, enum_name))
+            code += "    " + key + " = " + str(value)
+            code += ";\n"
+            names.append(key)
+        code += "\n\n"
+
+    return code
+
